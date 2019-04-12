02a7db885bc4551f70f8abb8ef6d68e547ef6124
shaanamin13 <aminshaan2810@gmail.com>
2019-04-12 15:38:47 -0700

    ethenolMix Needed Calc

--- e85Buddy/Services/CalculationService.cs
+++ e85Buddy/Services/CalculationService.cs
@@ -5,7 +5,11 @@ namespace E85Buddy.Services
     {
         public double EthCalc(double tankCapacity, double tankPercentage, double e85EthenolContent, double gasEthenolContent, double targetEthenolMix, double currentEthenolMix)
         {
-            throw new NotImplementedException();
+            double tankAmount = tankCapacity * (tankPercentage / 100);
+            double toFill = tankCapacity - tankAmount;
+
+            double ethenolPercentageNeeded = toFill / ((tankCapacity / targetEthenolMix) - (tankAmount / currentEthenolMix));
+            return ethenolPercentageNeeded;
         }
 
         public double GasCalc(double tankCapacity, double tankPercentage, double e85EthenolContent, double gasEthenolContent, double targetEthenolMix, double currentEthenolMix)
